f811232ea693e394759aae5fd1914643b9ce97bf
ibering <irina.bering@it-novum.com>
2025-06-23 13:18:49 +0200

    #ITC-3435 Organizational chart - Containers:  Alternative tree view [Browser] - organizationalCharts/index: fixed undefined error

--- src/Controller/OrganizationalChartsController.php
+++ src/Controller/OrganizationalChartsController.php
@@ -122,8 +122,8 @@ class OrganizationalChartsController extends AppController {
             $organizationalCharts[$index]['statesummary'] = '???';
         }
 
-        $this->set('all_organizationalCharts', $organizationalCharts);
-        $this->viewBuilder()->setOption('serialize', ['all_organizationalCharts']);
+        $this->set('all_organizationalcharts', $organizationalCharts);
+        $this->viewBuilder()->setOption('serialize', ['all_organizationalcharts']);
     }
 
     /**

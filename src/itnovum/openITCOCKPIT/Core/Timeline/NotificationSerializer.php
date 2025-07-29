6d22bf96cbbccea56a127a91fdbe1f82150ae645
nook24 <d.ziegler@allgeier-its.com>
2025-07-29 08:27:45 +0200

    ITC-3517 Add notification reason type into API response and timeline

--- src/itnovum/openITCOCKPIT/Core/Timeline/NotificationSerializer.php
+++ src/itnovum/openITCOCKPIT/Core/Timeline/NotificationSerializer.php
@@ -73,14 +73,18 @@ class NotificationSerializer {
         for ($i = 0; $i < $size; $i++) {
             if ($this->type === 'host') {
                 $notificationTime = $this->records[$i]['NotificationHost']->getStartTime();
+                $reasonType = $this->records[$i]['NotificationHost']->getReasonTypeString();
             } else {
                 $notificationTime = $this->records[$i]['NotificationService']->getStartTime();
+                $reasonType = $this->records[$i]['NotificationService']->getReasonTypeString();
             }
             $title = sprintf(
-                '<b class="not-xss-filtered-html">%s</b> via <b class="not-xss-filtered-html">%s</b> at %s',
+                '<b class="not-xss-filtered-html">%s</b> via <b class="not-xss-filtered-html">%s</b> at %s (%s: %s)',
                 h($this->records[$i]['Contact']->getName()),
                 h($this->records[$i]['Command']->getName()),
-                $this->UserTime->format($notificationTime)
+                $this->UserTime->format($notificationTime),
+                __('Reason'),
+                h($reasonType)
             );
 
             $records[] = [

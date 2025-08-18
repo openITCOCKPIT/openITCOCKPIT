2730402c767f74a17b5131b67ca2aea8e23b3c8b
abergen <a.bergen@allgeier-its.com>
2025-08-18 09:30:31 +0000

    ITC-3529 - added Dell iDrac Wizard to wizard list

--- src/Model/Table/WizardAssignmentsTable.php
+++ src/Model/Table/WizardAssignmentsTable.php
@@ -1,4 +1,27 @@
 <?php
+// Copyright (C) <2015-present>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
+
 declare(strict_types=1);
 
 namespace App\Model\Table;
@@ -349,6 +372,13 @@ class WizardAssignmentsTable extends Table {
                 'description' => __('These checks will allow you to monitor your HP server'),
                 'image'       => 'hp.svg',
                 'category'    => ['hardware'],
+            ],
+            'dell-idrac'             => [
+                'type_id'     => 'dell-idrac',
+                'title'       => __('Dell iDrac'),
+                'description' => __('Used to check Dell iDrac devices.'),
+                'image'       => 'dell.png',
+                'category'    => ['linux', 'network']
             ]
         ];
 

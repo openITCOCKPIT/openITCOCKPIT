8d2397053b2f568e43d2ec04fdc54c250a78764a
nook24 <d.ziegler@allgeier-its.com>
2025-08-20 10:39:20 +0200

    ITC-2801 Fix type in Even

--- src/Command/NagiosNotificationCommand.php
+++ src/Command/NagiosNotificationCommand.php
@@ -203,7 +203,7 @@ class NagiosNotificationCommand extends Command {
         $this->ticketsystemUrl = $_systemsettings['TICKET_SYSTEM']['TICKET_SYSTEM.URL'];
 
         // ITC-2801 We keep this for legacy reasons
-        // Event if the Acknowledgement via E-Mail has been removed, some Customers are may use a self written solution on Python or similar
+        // Even if the Acknowledgement via E-Mail has been removed, some Customers are may use a self written solution on Python or similar
         // and may rely on this "replyTo" address
         if (!empty($_systemsettings['MONITORING']['MONITORING.ACK_RECEIVER_ADDRESS'])) {
             $this->replyTo = $_systemsettings['MONITORING']['MONITORING.ACK_RECEIVER_ADDRESS'];

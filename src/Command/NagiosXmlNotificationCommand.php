7c58a4f371d9f64114e142387ecd08c2a28d2d6d
nook24 <d.ziegler@allgeier-its.com>
2025-08-20 10:32:10 +0200

    ITC-2801 Remove AcknowledgePerMailCommand

--- src/Command/NagiosXmlNotificationCommand.php
+++ src/Command/NagiosXmlNotificationCommand.php
@@ -4,18 +4,23 @@
 // This file is dual licensed
 //
 // 1.
-//	This program is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License
-//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 // 2.
 //	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
@@ -32,8 +37,8 @@ use App\itnovum\openITCOCKPIT\Core\EmailCharacters;
 use App\Model\Table\HostsTable;
 use App\Model\Table\ServicesTable;
 use App\Model\Table\SystemsettingsTable;
-use Cake\Console\Arguments;
 use Cake\Command\Command;
+use Cake\Console\Arguments;
 use Cake\Console\ConsoleIo;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Core\Plugin;
@@ -196,9 +201,9 @@ class NagiosXmlNotificationCommand extends Command {
         $this->systemAddress = $_systemsettings['SYSTEM']['SYSTEM.ADDRESS'];
         $this->ticketsystemUrl = $_systemsettings['TICKET_SYSTEM']['TICKET_SYSTEM.URL'];
 
-        if (!empty($_systemsettings['MONITORING']['MONITORING.ACK_RECEIVER_ADDRESS'])) {
-            $this->replyTo = $_systemsettings['MONITORING']['MONITORING.ACK_RECEIVER_ADDRESS'];
-        }
+        //if (!empty($_systemsettings['MONITORING']['MONITORING.ACK_RECEIVER_ADDRESS'])) {
+        //    $this->replyTo = $_systemsettings['MONITORING']['MONITORING.ACK_RECEIVER_ADDRESS'];
+        //}
 
 
         if ($this->type === 'host') {

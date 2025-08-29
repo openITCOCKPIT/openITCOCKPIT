899db2b7932871fc8b5fccc0d2cd7a97c9d02327
abergen <a.bergen@allgeier-its.com>
2025-08-29 07:48:03 +0000

    ITC-3529 - added trim to snmp parameters

--- src/itnovum/openITCOCKPIT/Core/SNMP/SNMPScan.php
+++ src/itnovum/openITCOCKPIT/Core/SNMP/SNMPScan.php
@@ -127,7 +127,7 @@ class SNMPScan {
         if (empty($this->snmpCommunity)) {
             throw new \InvalidArgumentException('Community name is missing');
         }
-        return ['--community', '"' . $this->snmpCommunity . '"'];
+        return ['--community', '"' . trim($this->snmpCommunity) . '"'];
     }
 
     /**
@@ -146,23 +146,23 @@ class SNMPScan {
         $snmpV3Credentials = ['--protocol 3'];
         if (!empty($this->securityName)) {
             $snmpV3Credentials[] = '--username';
-            $snmpV3Credentials[] = '"' . $this->securityName . '"';
+            $snmpV3Credentials[] = '"' . trim($this->securityName) . '"';
         }
         if (!empty($this->authPassword)) {
             $snmpV3Credentials[] = '--authpassword';
-            $snmpV3Credentials[] = '"' . $this->authPassword . '"';
+            $snmpV3Credentials[] = '"' . trim($this->authPassword) . '"';
         }
         if (!empty($this->authProtocol)) {
             $snmpV3Credentials[] = '--authprotocol';
-            $snmpV3Credentials[] = '"' . $this->authProtocol . '"';
+            $snmpV3Credentials[] = '"' . trim($this->authProtocol) . '"';
         }
         if (!empty($this->privacyPassword)) {
             $snmpV3Credentials[] = '--privpassword';
-            $snmpV3Credentials[] = '"' . $this->privacyPassword . '"';
+            $snmpV3Credentials[] = '"' . trim($this->privacyPassword) . '"';
         }
         if (!empty($this->privacyProtocol)) {
             $snmpV3Credentials[] = '--privprotocol';
-            $snmpV3Credentials[] = '"' . $this->privacyProtocol . '"';
+            $snmpV3Credentials[] = '"' . trim($this->privacyProtocol) . '"';
         }
 
         if (sizeof($snmpV3Credentials) === 1) {

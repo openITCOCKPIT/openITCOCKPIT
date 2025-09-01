35e7056b8516c1386f006969798d952db169f7eb
nook24 <d.ziegler@allgeier-its.com>
2025-09-01 08:19:40 +0200

    ITC-3560 remove AngularJS Support for ConfigurationFilesController

--- src/Lib/AclDependencies.php
+++ src/Lib/AclDependencies.php
@@ -627,7 +627,6 @@ class AclDependencies {
             ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'Statusengine3Cfg')
             ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'GraphiteWeb')
             ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'restorDefault')
-            ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'dynamicDirective')
             ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'NSTAMaster')
             ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'Gearman')
             ->dependency('ConfigurationFiles', 'edit', 'ConfigurationFiles', 'PhpFpmOitc');
